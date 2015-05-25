778599c08091327b8a845b006ad9d6dc9ea2cfbc
Dongyang Li <dongyang.li.cs@gmail.com>
2015-05-24 23:57:23 -0700

    init

--- Indexer.py
+++ Indexer.py
@@ -1,3 +1,8 @@
+#igraph_test.py
+# http://graus.nu/blog/pylucene-4-0-in-60-seconds-tutorial/
+# http://www.tutorialspoint.com/lucene/lucene_overview.htm
+
+import MySQLdb as mdb
 import sys
 import lucene
  
@@ -9,17 +14,36 @@ from org.apache.lucene.store import SimpleFSDirectory
 from org.apache.lucene.util import Version
 
 if __name__ == "__main__":
+	
 	lucene.initVM()
-	indexDir = SimpleFSDirectory(File("./indexDir"))
+	indexDir = SimpleFSDirectory(File("./articleTitleIndex"))
 	writerConfig = IndexWriterConfig(Version.LUCENE_4_10_1, StandardAnalyzer())
 	writer = IndexWriter(indexDir, writerConfig)
- 
-	print "%d docs in index" % writer.numDocs()
-	print "Reading lines from sys.stdin..."
-	for n, l in enumerate(sys.stdin):
-		doc = Document()
-		doc.add(Field("text", l, Field.Store.YES, Field.Index.ANALYZED))
-		writer.addDocument(doc)
-	print "Indexed %d lines from stdin (%d docs in index)" % (n, writer.numDocs())
+
+	try:
+		con = mdb.connect('localhost', 'root', '', 'cs246')
+		cur = con.cursor()
+		cur.execute("SELECT * FROM article_page;")
+		rows = cur.fetchall()
+		n = 0
+		for row in rows:
+			n = n+1
+			page_id = str(row[0])
+			page_title = str(row[1]).replace('_', ' ')
+
+			doc = Document()
+			doc.add(Field("title", page_title, Field.Store.NO, Field.Index.ANALYZED_NO_NORMS))
+			doc.add(Field("id", page_id, Field.Store.YES, Field.Index.NO))
+			writer.addDocument(doc)
+		print "total number of tuples", n
+	except mdb.Error, e:
+		print "Error %d: %s" % (e.args[0],e.args[1])
+		sys.exit(1)
+	finally:
+		if con:    
+			con.close()
+
+	print "Created (%d docs in index)" % (writer.numDocs())
 	print "Closing index of %d docs..." % writer.numDocs()
 	writer.close()
+
